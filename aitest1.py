500ce00bcd93d212cfddc860205c4e91d87b7151
LittleXuezha <wuxuemeng1993@163.com>
2019-04-24 15:22:05 +0800

    人工神经网络的反响传播算法，要根据数学公式，但是我建议推到后记住，因为不同层有不同的公式，但是上一层依赖下一层。 也可以直接用已经写好的框架

--- aitest1.py
+++ aitest1.py
@@ -6,9 +6,13 @@ def sigmoid(z):
     return 1/(1+np.exp(-z))
 
 
-def loss(ytrue, ypred):
+def mse_loss(ytrue, ypred):
     return ((ytrue - ypred)**2).mean()
 
+
+def deriv_sigmoid(x):
+    fx = sigmoid(x)
+    return fx * (1-fx)
 
 class Neuron:
     def __init__(self, w, b):
@@ -21,28 +25,103 @@ class Neuron:
         return sigmoid(total)
 
 
-weights = np.array([0, 1])
-bias = 4
-n = Neuron(weights, bias)
-final = n.feedforward(np.array([2, 3]))
-print(final)
-print('---------------')
-
-
 class OurNeuronNet:
     def __init__(self):
-        w = np.array([0, 1])
-        b = 0
-        self.h1 = Neuron(w, b)
-        self.h2 = Neuron(w, b)
-        self.o1 = Neuron(w, b)
+        # 不同的权重
+        self.w1 = np.random.normal()
+        self.w2 = np.random.normal()
+        self.w3 = np.random.normal()
+        self.w4 = np.random.normal()
+        self.w5 = np.random.normal()
+        self.w6 = np.random.normal()
+
+        # 不同的偏执权重
+        self.b1 = np.random.normal()
+        self.b2 = np.random.normal()
+        self.b3 = np.random.normal()
 
     def feedforward(self, x):
-        out_h1 = self.h1.feedforward(x)
-        out_h2 = self.h2.feedforward(x)
-        out_o1 = self.o1.feedforward(np.array([out_h1, out_h2]))
-        return out_o1
+        h1 = sigmoid(self.w1 * x[0] + self.w2 * x[1] +self.b1)
+        h2 = sigmoid(self.w3 * x[0] + self.w4 * x[1] + self.b2)
+        o1 = sigmoid(self.w5 * h1 + self.w6 * h2 + self.b3)
+        return o1
+
+    def train(self, data, all_y_true):
+        learn_rate = 0.1
+        epochs = 1000
+
+        for epoch in range(epochs):
+            for x, y_true in zip(data, all_y_true):
+                # 前向传播，过会需要这个值
+                sum_h1 = self.w1 * x[0] + self.w2 * x[1] + self.b1
+                h1 = sigmoid(sum_h1)
+
+                sum_h2 = self.w3 * x[0] + self.w4 * x[1] + self.b2
+                h2 = sigmoid(sum_h2)
+
+                sum_o1 = self.w5 * h1 + self.w6 * h2 + self.b3
+                o1 = sigmoid(sum_o1)
+
+                y_pred = o1
+                d_L_d_ypred = -2*(y_true-y_pred)
+
+                # nauron o1 的他w
+                d_ypred_d_w5 = h1 * deriv_sigmoid(sum_o1)
+                d_ypred_d_w6 = h2 * deriv_sigmoid(sum_o1)
+                d_ypred_d_b3 = deriv_sigmoid(sum_o1)
+
+                d_ypred_d_h1 = self.w5 * deriv_sigmoid(sum_o1)
+                d_ypred_d_h2 = self.w6 * deriv_sigmoid(sum_o1)
+
+                # neuron h1
+                d_h1_d_w1 = x[0] * deriv_sigmoid(sum_h1)
+                d_h1_d_w2 = x[1] * deriv_sigmoid(sum_h1)
+                d_h1_d_b1 = deriv_sigmoid(sum_h1)
+
+                #neuron h2
+                d_h1_d_w3 = x[0] * deriv_sigmoid(sum_h2)
+                d_h1_d_w4 = x[1] * deriv_sigmoid(sum_h2)
+                d_h1_d_b2 = deriv_sigmoid(sum_h2)
+
+                # 更新w和b
+                # neuron h1
+                self.w1 -=learn_rate*d_L_d_ypred*d_ypred_d_h1*d_h1_d_w1
+                self.w2 -= learn_rate * d_L_d_ypred * d_ypred_d_h1 * d_h1_d_w2
+                self.b1 -=learn_rate*d_L_d_ypred*d_ypred_d_h1*d_h1_d_b1
+                # neuron h2
+                self.w3 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h1_d_w3
+                self.w4 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h1_d_w4
+                self.b1 -= learn_rate * d_L_d_ypred * d_ypred_d_h2 * d_h1_d_b2
+
+                # neuron o1
+                self.w5 -= learn_rate * d_L_d_ypred * d_ypred_d_w5
+                self.w6 -= learn_rate * d_L_d_ypred * d_ypred_d_w6
+                self.b3 -= learn_rate * d_L_d_ypred * d_ypred_d_b3
+
+                if epoch % 10==0:
+                    y_preds = np.apply_along_axis(self.feedforward,1, data)
+                    loss = mse_loss(all_y_true, y_preds)
+                    print('epoch %d loss %.3f' %(epoch, loss))
 
 
-nn = OurNeuronNet()
-print(nn.feedforward(np.array([2, 3])))
+data = np.array([
+    [-2, -1],
+    [25, 6],
+    [17, 4],
+    [-15, -6]
+])
+# 0代表男
+all_y_true = np.array([
+    1,
+    0,
+    0,
+    1
+])
+# 创建神经网络，并训练，反向传播
+network = OurNeuronNet()
+network.train(data, all_y_true)
+# 插入数据预测，接近0为男 0.94,0.04
+emily = np.array([-7, -3])
+frank = np.array([20, 2])
+print("emily: %.3f", network.feedforward(emily))
+print("frank: %.3f", network.feedforward(frank))
